966ab778ab67f0984b9a8d78786a0855eafa3884
Tan Minh <minhtandz951@gmail.com>
2023-01-08 19:19:12 +0700

    up code master to mer code charts

--- app/controller/APIs/API.js
+++ app/controller/APIs/API.js
@@ -1,7 +1,7 @@
 export default class API {
-  static headers = {
-    XRapidAPIKey: 'pastapi',
-    XRapidAPIHost: 'api-football-v1.p.rapidapi.com',
-    timezone: 'Asia/Ho_Chi_Minh',
-  };
+    static headers = {
+        XRapidAPIKey: 'eb528ed8bamshd58b016b9a2cff2p10eb27jsnc58b48e4f4fa',
+        XRapidAPIHost: 'api-football-v1.p.rapidapi.com',
+        timezone: 'Asia/Ho_Chi_Minh',
+    };
 }
