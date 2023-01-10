dc9012dc41a985574c6288b05fb8b23c303fb9c2
vanhoan01 <nguyenvanhoan251201@gmail.com>
2023-01-10 11:05:49 +0700

    ud

--- app/controller/APIs/API.js
+++ app/controller/APIs/API.js
@@ -1,8 +1,6 @@
 export default class API {
   static headers = {
-    XRapidAPIKey: 'ba9f6cc2aamsh10400d1a4ae0300p1ecab2jsn7ada17d2a07f',
-    //2: ba9f6cc2aamsh10400d1a4ae0300p1ecab2jsn7ada17d2a07f
-    //1: 839f30d8fbmsh8a0057962cf6f3ap125668jsn9d19dc20d605
+    XRapidAPIKey: 'pastapi',
     XRapidAPIHost: 'api-football-v1.p.rapidapi.com',
     timezone: 'Asia/Ho_Chi_Minh',
   };
