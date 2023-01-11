e0b12eba0e1da392d50cc55aedd39fd66e1b857b
vanhoan01 <nguyenvanhoan251201@gmail.com>
2023-01-11 15:38:26 +0700

    ud

--- app/controller/APIs/API.js
+++ app/controller/APIs/API.js
@@ -1,6 +1,6 @@
 export default class API {
   static headers = {
-    XRapidAPIKey: 'ba9f6cc2aamsh10400d1a4ae0300p1ecab2jsn7ada17d2a07f',
+    XRapidAPIKey: 'pastapi',
     XRapidAPIHost: 'api-football-v1.p.rapidapi.com',
     timezone: 'Asia/Ho_Chi_Minh',
   };
