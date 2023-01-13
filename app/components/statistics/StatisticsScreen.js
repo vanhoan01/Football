305161c821df3abea8948132312c6a75033275cd
NgoDinhHoangHa <hoanghadn0123@gmail.com>
2023-01-13 17:21:49 +0700

    statistics

--- app/components/statistics/StatisticsScreen.js
+++ app/components/statistics/StatisticsScreen.js
@@ -1,28 +1,186 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
-  Text,
   FlatList,
   StyleSheet,
+  Text,
   Image,
   TouchableOpacity,
-  ActivityIndicator,
 } from 'react-native';
-
+import Constant from '../../controller/Constant';
+import {useNavigation} from '@react-navigation/native';
+let league = [1];
 const StatisticsScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Số liệu thống kê</Text>
-    </View>
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Score');
+        }}>
+        <View
+          style={{
+            width: '90%',
+            height: 100,
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            marginVertical: 20,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 20,
+          }}>
+          <View
+            style={{
+              width: '30%',
+              // backgroundColor: 'yellow',
+            }}>
+            <Image style={styles.logo} source={Constant.images.score} />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.txt}>TOP SCORE</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Assist');
+        }}>
+        <View
+          style={{
+            width: '90%',
+            height: 100,
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            marginVertical: 20,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 20,
+          }}>
+          <View
+            style={{
+              width: '30%',
+              // backgroundColor: 'yellow',
+            }}>
+            <Image style={styles.logo} source={Constant.images.assists} />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.txt}>TOP ASSISTS</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('YellowCard');
+        }}>
+        <View
+          style={{
+            width: '90%',
+            height: 100,
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            marginVertical: 20,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 20,
+          }}>
+          <View
+            style={{
+              width: '30%',
+              // backgroundColor: 'yellow',
+            }}>
+            <Image style={styles.logo} source={Constant.images.yellowcard} />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.txt}>TOP YELLOW CARD</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('RedCard');
+        }}>
+        <View
+          style={{
+            width: '90%',
+            height: 100,
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            marginVertical: 20,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 20,
+          }}>
+          <View
+            style={{
+              width: '30%',
+              // backgroundColor: 'yellow',
+            }}>
+            <Image style={styles.logo} source={Constant.images.redcard} />
+          </View>
+
+          <View style={styles.container}>
+            <Text style={styles.txt}>TOP RED CARD</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </>
   );
 };
 
 export default StatisticsScreen;
-
 const styles = StyleSheet.create({
-  imageThumbnail: {
-    height: 130,
-    width: undefined,
-    resizeMode: 'stretch',
+  container: {
+    width: '60%',
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    padding: 5,
+    // marginTop: 5,
+    marginLeft: 10,
+    width: 100,
+    height: 100,
+
+    resizeMode: 'contain',
+  },
+  txt: {
+    color: 'black',
+    fontSize: 13,
+    fontWeight: 'normal',
+    fontWeight: 'bold',
+  },
+  rootView: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  list: {
+    flex: 1,
+  },
+  firstText1: {
+    fontSize: 14,
+    margin: 10,
+    fontStyle: 'normal',
+    color: 'black',
+  },
+  viewH: {
+    flexDirection: 'row',
+    height: 40,
+    backgroundColor: Constant.color.background,
+    // flex: 1,
+  },
+  viewG: {
+    alignItems: 'flex-end',
+    flex: 1,
+  },
+  viewP: {
+    alignItems: 'flex-start',
   },
 });
--- app/controller/Constant.js
+++ app/controller/Constant.js
@@ -13,6 +13,10 @@ export default {
     img595097: require('../assets/images/img_595097.jpg'),
     mercedes: require('../assets/images/img_mercedes.jpg'),
     bmw: require('../assets/images/img_bmw.png'),
+    score: require('../assets/images/score.png'),
+    assists: require('../assets/images/98611.png'),
+    yellowcard: require('../assets/images/yellowcard.png'),
+    redcard: require('../assets/images/redcard.png'),
   },
   fonts: {
     notoSansJPBold: 'NotoSansJP-Bold',
